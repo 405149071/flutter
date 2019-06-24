2d29b6b54b85b5ac9048a2ac583fb45611a6e3de
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-06-24 12:21:08 +0800

    引入fluttertoast实现toast弹出

--- flutter_app03/lib/pages/user.dart
+++ flutter_app03/lib/pages/user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({Key key}) : super(key: key);
@@ -9,6 +10,22 @@ class UserPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text("用户中心"),
+        ),
+        body: RaisedButton(
+          child: Text("按钮"),
+          onPressed: () {
+            //Fluttertoast.showToast();
+            print("aabbcc");
+            // 需要 flutter clean ,然后重新启动才行，热启动是不行的
+            Fluttertoast.showToast(
+                msg: "Toast提示信息",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                timeInSecForIos: 1,
+                backgroundColor: Colors.red,
+                textColor: Colors.white,
+                fontSize: 16.0);
+          },
         ),
       ),
     );
